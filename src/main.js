626ff89b182d943a1b1fbae9c5eafbe9204817bc
wen2 <wen922@gmail.com>
2020-08-20 01:06:21 +0800

    試寫

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+//---- CSS
+import 'bootstrap/dist/css/bootstrap.css'
+import '../src/less/style_admin.less'
+
+createApp(App).use(router).mount('#app')
+
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="../assets/logo.png">
+
+      這是首頁
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home',
+  components: {
+
+  }
+}
+</script>
+
+
