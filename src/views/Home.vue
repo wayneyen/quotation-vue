8b24863c2e77d7d1eda42506a3edacd8a659c202
張雯雯 <>
2020-08-27 18:21:20 +0800

    拖拉組件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <div id="app">
+        <img alt="Vue logo" src="../assets/logo.png">
 
-      這是首頁
-  </div>
+          這是首頁
+        <draggable class="drag-list">
+        <div class="con">
+          <i class="oin-icon oin_lines"/>
+          <div class="name">
+            項目Ａ
+          </div>
+        </div>
+        <div class="con">
+          <i class="oin-icon oin_lines"/>
+          <div class="name">
+            項目Ｂ
+          </div>
+        </div>
+      </draggable>
+    </div>
 </template>
 
 <script>
 
+  import draggable from 'vuedraggable'
+  export default {
+      name: 'Home',
+      components: {
+          draggable,
+      },
+      data() {
+          return {
 
-export default {
-  name: 'Home',
-  components: {
+          }
+      },
+      mounted() {
 
+      },
+      methods: {
+      }
   }
-}
 </script>
 
 
